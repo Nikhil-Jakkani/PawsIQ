21e1dc078a5256af98e39e45cbb4057ff706038a
Nikhil Jakkani <jakkaninikhil2003@gmail.com>
2025-07-25 20:40:52 +0530

    backend for storing ai suggestions

--- server/src/api/routes/index.ts
+++ server/src/api/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import adminAuthRoutes from './admin/auth.route.js';
 import adminUserRoutes from './admin/user.route.js';
 import userAiRoutes from './user/ai.route.js';
+import userSuggestionRoutes from './user/suggestion.route.js';
 
 const router = Router();
 
 router.use('/admin/auth', adminAuthRoutes);
 router.use('/admin/users', adminUserRoutes);
 router.use('/user/ai', userAiRoutes);
+router.use('/user/suggestions', userSuggestionRoutes);
 
 export default router;
