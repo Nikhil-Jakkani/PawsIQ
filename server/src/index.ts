d5171f17169fb38d1e652062ae86853b2c974131
Nikhil Jakkani <jakkaninikhil2003@gmail.com>
2025-08-15 13:13:36 +0530

    port routing

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,8 @@ import app from './app.js';
 import config from './config/config.js';
 
 const port = config.port;
+const host = '0.0.0.0';
 
-app.listen(port, () => {
-  console.log(`PawsIQ Backend listening on port ${port}`);
+app.listen(port, host, () => {
+  console.log(`PawsIQ Backend listening on http://${host}:${port}`);
 });
